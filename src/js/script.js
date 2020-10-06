dd244e2a7004872fe81ffcc4d95f0eb94b85da94
Dany-Trudel <70211907+Dany-Trudel@users.noreply.github.com>
2020-10-06 13:38:35 -0400

    fini

--- src/js/script.js
+++ src/js/script.js
@@ -3,4 +3,32 @@ document.addEventListener("DOMContentLoaded", function(event) {
     //Un commentaire
     console.log("Ça fonctionne!!!");
 
+    var hamburgers = document.querySelectorAll('.menu-hamburger');
+    var menuMobile = document.querySelector(".nav-primary-mobile");
+
+    for(let i = 0; i < hamburgers.length; i++ ){
+        var hamburgers =  hamburgers[i];
+        hamburgers.addEventListener('click', openMenu);
+    }
+
+
+    function openMenu(evt) {
+        console.log('click');
+        evt.preventDefault();
+
+        var cible = evt.currentTarget;
+
+
+        if(cible.classList.contains('open')){
+            cible.classList.remove('open');
+        } else {
+            cible.classList.add("open");
+        }
+
+    }
+
+
+
+
+
 });
